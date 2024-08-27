1e98ae0bbd2e2781125b334ce9e89016c3a0adb7
mohammad <usermp76@gmail.com>
2024-08-27 12:33:39 +0330

    Update option to long text

--- migrations/2024_07_09_074540_create_media_table.php
+++ migrations/2024_07_09_074540_create_media_table.php
@@ -14,7 +14,7 @@ class CreatemediaTable extends Migration
             $table->string('alt')->nullable();
             $table->unsignedBigInteger('user_id')->nullable();
             $table->text('description')->nullable();
-            $table->string('option')->nullable();
+            $table->longText('option')->nullable();
             $table->string('path');
             $table->timestamps();
             $table->softDeletes();
